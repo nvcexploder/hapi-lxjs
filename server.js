c425d62aa2c2125f7e6bfe8674623e079dc90830
nvcexploder <benacker@gmail.com>
2014-06-27 13:12:45 +0100

    Adding participant plugins to server

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 var Hapi = require('hapi');
+var FridayParticipants = require('./friday-participants.json');
+
 var server = Hapi.createServer('localhost', 8080);
 
-var ana = require('./lib/plugins/ana-rodrigues');
 
-server.start();
+FridayParticipants.forEach(function (participant) {
+
+	var participantPlugin = require('./lib/plugins/' + participant);
+	server.pack.register(participantPlugin, function (err) {
+
+		if (err) {
+			console.error(err);
+			process.exit();
+		}
+	});
+});
+
+server.start(function (err) {
+
+	if (err) {
+		console.error(err);
+		return;
+	}
+
+	console.log('Server started');
+});
