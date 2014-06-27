3e95602487cc8612b658d15190bb8bdd4cc46957
Ricardo Quintas <rqcenter-github@yahoo.com>
2014-06-27 15:47:22 +0100

    added my user id to friday-participants.json

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 var Hapi = require('hapi');
 var FridayParticipants = require('./friday-participants.json');
 
-var server = Hapi.createServer('0.0.0.0', 8080);
+var server = Hapi.createServer('localhost', 8080);
 
 
-FridayParticipants.forEach(function (participant) {
+FridayParticipants.forEach(function(participant) {
 
-	var participantPlugin = require('./lib/plugins/' + participant);
-	server.pack.register(participantPlugin, function (err) {
+    var participantPlugin = require('./lib/plugins/' + participant);
+    console.log('participant: ' + participant);
+    server.pack.register(participantPlugin, function(err) {
 
-		if (err) {
-			console.error(err);
-			process.exit();
-		}
-	});
+        if (err) {
+            console.error(err);
+            process.exit();
+        }
+    });
 });
 
-server.start(function (err) {
+server.start(function(err) {
 
-	if (err) {
-		console.error(err);
-		return;
-	}
+    if (err) {
+        console.error(err);
+        return;
+    }
 
-	console.log('Server started');
+    console.log('Server started');
 });
